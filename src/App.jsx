6b14a86004b01ae093ce614902788ec234d3ca9e
Pedro Kusisakana <49099181+pedrokusssy@users.noreply.github.com>
2026-04-27 11:29:35 +0100

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,47 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Importa o teu Layout e as tuas Páginas
-import MainLayout from "./components/MainLayout"; 
-import Login from "./pages/Login"; // (Ajusta os caminhos se necessário)
+import { ProtectedRoute } from "./ProtectedRoute";
+import { DiarioProvider } from "./contexts/DiarioContext";
+// Importa Layout e as Páginas
+import MainLayout from "./components/MainLayout";
+import Login from "./pages/Login";
 import DiarioList from "./pages/DiarioList";
-// import Formacao from "./pages/Formacao";
-// import NovoDiario from "./pages/NovoDiario";
+import FormacaoList from "./pages/Formacao";
+import Diario from "./pages/Diario";
+import MinhaConta from "./pages/minhaConta";
 
 function App() {
   return (
     <ChakraProvider>
-      <Router>
-        <Routes>
-          {/* 1. ROTAS SEM MOLDURA (Aparecem em ecrã inteiro) */}
-          <Route path="/" element={<Login />} />
-          {/* Se tiveres página de Registo ou Esqueceu a Palavra-passe, põe aqui */}
+      <DiarioProvider>
+        <Router>
+          <Routes>
+            {/* 1. ROTAS SEM MOLDURA (Aparecem em ecrã inteiro) */}
+            <Route path="/" element={<Login />} />
+            {/* Se tiveres página de Registo ou Esqueceu a Palavra-passe, põe aqui */}
 
-          {/* 2. ROTAS COM MOLDURA (Dashboard) */}
-          <Route
-            path="/*"
-            element={
-              <MainLayout>
-                <Routes>
-                  {/* Todas as tuas páginas do sistema entram aqui! */}
-                  <Route path="/diarios" element={<DiarioList />} />
-                  {/* <Route path="/novoDiario" element={<NovoDiario />} /> */}
-                  {/* <Route path="/editarDiario/:id" element={<EditarDiario />} /> */}
-                  {/* <Route path="/formacao" element={<Formacao />} /> */}
-                </Routes>
-              </MainLayout>
-            }
-          />
-        </Routes>
-      </Router>
+            {/* 2. ROTAS COM MOLDURA (Dashboard) */}
+            <Route
+              path="/*"
+              element={
+                <MainLayout>
+                  <ProtectedRoute>
+                    <Routes>
+                      {/* Todas as páginas do sistema entram aqui! com rotas protegidas */}!
+                      <Route path="/novoDiario" element={<ProtectedRoute><Diario /></ProtectedRoute>} />
+                      <Route path="/diarios" element={<ProtectedRoute><DiarioList /></ProtectedRoute>} />
+                      <Route path="/editarDiario/:id" element={<ProtectedRoute><Diario /></ProtectedRoute>} />
+                      <Route path="/minha-conta" element={<ProtectedRoute><MinhaConta /></ProtectedRoute>} />
+                      <Route path="/formacao" element={<ProtectedRoute><FormacaoList /></ProtectedRoute>} />
+                    </Routes>
+                  </ProtectedRoute>
+                </MainLayout>
+              }
+            />
+          </Routes>
+        </Router>
+      </DiarioProvider>
     </ChakraProvider>
   );
 }
