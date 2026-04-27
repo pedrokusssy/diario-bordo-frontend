0b081f28701edb14a7cc1781c6b09eeb48873638
Pedro Kusisakana <49099181+pedrokusssy@users.noreply.github.com>
2026-04-27 19:15:33 +0100

    fix import on pord

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { ProtectedRoute } from "./ProtectedRoute";
 import { DiarioProvider } from "./contexts/DiarioContext";
+
 // Importa Layout e as Páginas
-import { MainLayout } from "./components/MainLayout";
-import { Login } from "./pages/Login";
-import {DiarioList} from "./pages/DiarioList";
-import {FormacaoList} from "./pages/Formacao";
-import {Diario} from "./pages/Diario";
-import {MinhaConta} from "./pages/minhaConta";
+import MainLayout from "./components/MainLayout";
+import Login from "./pages/Login";
+import DiarioList from "./pages/DiarioList";
+import FormacaoList from "./pages/Formacao";
+import Diario from "./pages/Diario";
+// Garante que o ficheiro na pasta se chama MinhaConta.jsx (com "M" maiúsculo)
+import MinhaConta from "./pages/MinhaConta"; 
 
 function App() {
   return (
@@ -19,24 +21,24 @@ function App() {
           <Routes>
             {/* 1. ROTAS SEM MOLDURA (Aparecem em ecrã inteiro) */}
             <Route path="/" element={<Login />} />
-            {/* Se tiveres página de Registo ou Esqueceu a Palavra-passe, põe aqui */}
 
-            {/* 2. ROTAS COM MOLDURA (Dashboard) */}
+            {/* 2. ROTAS COM MOLDURA (Dashboard - Protegidas) */}
             <Route
               path="/*"
               element={
-                <MainLayout>
-                  <ProtectedRoute>
+                /* O ProtectedRoute protege logo TUDO o que está aqui dentro */
+                <ProtectedRoute>
+                  <MainLayout>
                     <Routes>
-                      {/* Todas as páginas do sistema entram aqui! com rotas protegidas */}!
-                      <Route path="/novoDiario" element={<ProtectedRoute><Diario /></ProtectedRoute>} />
-                      <Route path="/diarios" element={<ProtectedRoute><DiarioList /></ProtectedRoute>} />
-                      <Route path="/editarDiario/:id" element={<ProtectedRoute><Diario /></ProtectedRoute>} />
-                      <Route path="/minha-conta" element={<ProtectedRoute><MinhaConta /></ProtectedRoute>} />
-                      <Route path="/formacao" element={<ProtectedRoute><FormacaoList /></ProtectedRoute>} />
+                      {/* Já não precisamos de repetir o ProtectedRoute em cada página! */}
+                      <Route path="/novoDiario" element={<Diario />} />
+                      <Route path="/diarios" element={<DiarioList />} />
+                      <Route path="/editarDiario/:id" element={<Diario />} />
+                      <Route path="/minha-conta" element={<MinhaConta />} />
+                      <Route path="/formacao" element={<FormacaoList />} />
                     </Routes>
-                  </ProtectedRoute>
-                </MainLayout>
+                  </MainLayout>
+                </ProtectedRoute>
               }
             />
           </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // Usando o React.StrictMode nativo
-    <ChakraProvider>
-        <App />
-    </ChakraProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
